0800b2246d4d8d776b71a3aa4f30ddf186eee62a
Mouthree <2115932130@qq.com>
2026-05-22 23:18:25 +0800

    增: 列表基础信息

---
+++ src/model/list.rs
@@ -0,0 +1,22 @@
+use derive_builder::Builder;
+use serde::{Deserialize, Serialize};
+
+
+///列表存储数据结构
+#[derive(Debug, Clone, Serialize, Deserialize, Builder)]
+pub struct TodoListData {
+    ///列表id
+    pub id: u64,
+    ///列表名字
+    pub name: String,
+    ///创建时间
+    pub create_time: i64,
+    ///最近打开时间
+    pub last_open: i64,
+    ///完成情况
+    pub current_state: Option<f64>,
+    ///列表注释
+    pub note: Option<String>,
+    ///优先级
+    pub priority: Option<u8> 
+}
