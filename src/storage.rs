13f2024e0569501fa798d235f3cd3528b34f3128
Mouthree <2115932130@qq.com>
2026-05-23 19:04:53 +0800

    增: 获取多个列表&项

--- src/storage.rs
+++ src/storage.rs
@@ -1,6 +1,7 @@
 //!数据库操作
 #![allow(dead_code)]
-use anyhow::Result;
+use anyhow:: Result};
+
 use sled::{Db, Tree};
 use crate::model::{item::ItemData, list::TodoListData};
 
@@ -25,17 +26,17 @@ impl Storage {
     }
 
     ///创建列表id
-    fn create_id_list(&self) -> Result<u64> {
+    pub fn create_id_list(&self) -> Result<u64> {
         Ok(self.db.generate_id()?)
     }
 
     ///创建项id
-    fn create_id_item(&self, list_id: u64) -> Result<(u64, u64)> {
+    pub fn create_id_item(&self, list_id: u64) -> Result<(u64, u64)> {
         Ok((list_id, self.db.generate_id()?))
     }
 
     ///保存或创建列表
-    fn save_or_new_list(&self, list: &TodoListData) -> Result<()> {
+    pub fn save_or_new_list(&self, list: &TodoListData) -> Result<()> {
         let id = list.id;
         let list = serde_json::to_vec(list)?;
         self.list_tree.insert(id.to_be_bytes(), list)?;
@@ -43,12 +44,36 @@ impl Storage {
     }
 
     ///保存或创建项
-    fn save_or_new_item(&self, item: &ItemData) -> Result<()> {
+    pub fn save_or_new_item(&self, item: &ItemData) -> Result<()> {
         let id = item.id;
         let id = [id.0.to_be_bytes(), id.1.to_be_bytes()].concat();
         let item = serde_json::to_vec(item)?;
         self.list_tree.insert(id, item)?;
         Ok(())
     }
+
+    ///获取所有列表
+    pub fn get_all_list(&self) -> Result<Vec<TodoListData>> {
+        let list = self.list_tree.iter()
+            .map(|m| {
+                let (_, value) = m?;
+                Ok(serde_json::from_slice(&value)?)
+            })
+            .collect();
+        list
+    }
+
+    ///获取列表中所有项
+    pub fn get_item_from_list(&self, list_id: u64) -> Result<Vec<ItemData>> {
+        let list_id = list_id.to_be_bytes();
+        let item = self.item_tree.iter()
+            .map(|m| {
+                let (_, value) = m?;
+                Ok(serde_json::from_slice(&value)?)
+            })
+            .collect();
+        item
+    }
+
     
 }
