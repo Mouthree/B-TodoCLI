727f066bb5dd998cbb1e9c28d91882e53c83a32e
Mouthree <2115932130@qq.com>
2026-05-23 15:15:52 +0800

    增: 创建列表&项, 使用anyhow进行报错

--- src/storage.rs
+++ src/storage.rs
@@ -1,39 +1,54 @@
 //!数据库操作
-
+#![allow(dead_code)]
+use anyhow::Result;
 use sled::{Db, Tree};
-use thiserror::Error;
-
-#[derive(Debug, Error)]
-pub enum StorageError {
-    #[error("数据库操作失败: {0}")]
-    Sled(#[from] sled::Error),
-}
+use crate::model::{item::ItemData, list::TodoListData};
 
 
 pub struct Storage {
     list_tree: Tree,
     item_tree: Tree,
-    pub db: Db
+    pub db: Db,
 }
 impl Storage {
-    pub fn new(path: &str) -> Result<Self, StorageError> {
+    pub fn new(path: &str) -> Result<Self> {
         let db = sled::open(path)?;
         let list_tree = db.open_tree("list")?;
         let item_tree = db.open_tree("item")?;
         Ok(
-            Storage { list_tree, item_tree, db }
+            Storage {
+                list_tree,
+                item_tree,
+                db,
+            }
         )
     }
-    
+
     ///创建列表id
-    fn create_id_list(&self) -> Result<u64, StorageError> {
+    fn create_id_list(&self) -> Result<u64> {
         Ok(self.db.generate_id()?)
     }
-    
+
     ///创建项id
-    fn create_id_item(&self, list_id: u64) -> Result<(u64, u64), StorageError> {
+    fn create_id_item(&self, list_id: u64) -> Result<(u64, u64)> {
         Ok((list_id, self.db.generate_id()?))
     }
 
+    ///保存或创建列表
+    fn save_or_new_list(&self, list: &TodoListData) -> Result<()> {
+        let id = list.id;
+        let list = serde_json::to_vec(list)?;
+        self.list_tree.insert(id.to_be_bytes(), list)?;
+        Ok(())
+    }
+
+    ///保存或创建项
+    fn save_or_new_item(&self, item: &ItemData) -> Result<()> {
+        let id = item.id;
+        let id = [id.0.to_be_bytes(), id.1.to_be_bytes()].concat();
+        let item = serde_json::to_vec(item)?;
+        self.list_tree.insert(id, item)?;
+        Ok(())
+    }
     
 }
