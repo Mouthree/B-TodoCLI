1486c896992ff2f8c48aba4b43afa98a0bbf461f
Mouthree <2115932130@qq.com>
2026-05-24 13:38:17 +0800

    改: 重新划分new的逻辑, 方便测试, 解耦

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 //!配置文件管理
 #![allow(dead_code)]
-use std::{fs::{self}, path::{Path}};
+use std::{fs::{self}, path::PathBuf};
 
 use anyhow::{Context, Result};
 use directories::ProjectDirs;
@@ -14,39 +14,68 @@ use tracing::info;
 pub struct AppConfig {
     test: String
 }
-
 impl AppConfig {
     pub fn new() -> Result<Self> {
-        info!("开始加载配置");
-        let proj_dirs = ProjectDirs::from("com", "MtrM", "mrTodo")
-            .context("无法确定用户配置目录")?;
-        let config_dir = proj_dirs.config_dir();          // &Path
-        let config_file = config_dir.join("config.toml"); // 配置文件完整路径
-
-        info!("配置目录: {}", config_dir.display());
+        let config_file = Self::set_config_file()?;
 
         if config_file.is_file() {
-            info!("找到已有配置文件，开始读取");
-            let contents = std::fs::read_to_string(&config_file)
-                .with_context(|| format!("无法读取配置文件 {}", config_file.display()))?;
-            let config: AppConfig = toml::from_str(&contents)
-                .context("配置文件格式错误")?;
-            Ok(config)
+            Self::get_local_config(&config_file)
         } else {
             info!("未找到配置文件，创建默认配置");
-            let default_config = AppConfig {
-                test: String::from("yeah")
-            };
+            let default_config = AppConfig::default();
             let toml_string = toml::to_string_pretty(&default_config)?;
-            if let Some(parent) = config_file.parent() {
-                fs::create_dir_all(parent)
-                    .with_context(|| format!("无法创建配置目录 {}", parent.display()))?;
-            }
-            fs::write(&config_file, &toml_string)
-                .with_context(|| format!("无法写入配置文件 {}", config_file.display()))?;
-
-            info!("默认配置文件已创建: {}", config_file.display());
+            Self::create_default_config_path(&config_file)?;
+            Self::write_default_config_file(&config_file, toml_string)?;
             Ok(default_config)
         }
     }
+
+    ///设置本地config路径
+    fn set_config_file() -> Result<PathBuf> {
+        let dirs = ProjectDirs::from("com", "MtrM", "mrTodo")
+            .context("无法确定用户配置目录")?;
+        let path = dirs.config_dir().join("config.toml");
+        info!("配置路径: {}", path.display());
+        Ok(path)
+    }
+
+    ///获取本地数据
+    fn get_local_config(config_file: &PathBuf) -> Result<AppConfig> {
+        info!("读取已有配置文件");
+        let contents = std::fs::read_to_string(config_file)
+            .with_context(|| format!("无法读取配置文件 {}", config_file.display()))?;
+        let config: AppConfig = toml::from_str(&contents)
+            .context("配置文件格式错误")?;
+        info!("读取成功");
+        Ok(config)
+    }
+
+    ///创建本地文件路径
+    fn create_default_config_path(config_file: &PathBuf) -> Result<()> {
+        if let Some(path) = config_file.parent() {
+            fs::create_dir_all(path)
+                .with_context(|| format!("无法创建配置目录 {}", path.display()))?;
+            info!("创建目录: {}", path.display());
+        }
+        Ok(())
+    }
+
+    ///写入默认配置文件
+    fn write_default_config_file(config_file: &PathBuf, toml_string: String) -> Result<()> {
+        fs::write(config_file, &toml_string)
+            .with_context(|| format!("无法写入配置文件 {}", config_file.display()))?;
+        info!("默认配置文件已创建: {}", config_file.display());
+        Ok(())
+    }
+}
+
+impl Default for AppConfig {
+    fn default() -> Self {
+        Self { test: Default::default() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    
 }
