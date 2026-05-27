bd725c971b541de61b0324cd1176cffb4648a1ed
Mouthree <2115932130@qq.com>
2026-05-27 23:59:19 +0800

    改: 完善结构体

--- src/ai.rs
+++ src/ai.rs
@@ -5,11 +5,18 @@ use serde::{Deserialize, Serialize};
 struct ChatRequest {
     model: String,
     message: Vec<Message>,
-    stream: bool
+    stream: bool,
+    thinking: EnableType,
+    reasoning_effort: String,
 }
 
 #[derive(Serialize, Debug, Deserialize, Clone)]
 struct Message {
     role: String,
     content: String
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct EnableType {
+    
 }
