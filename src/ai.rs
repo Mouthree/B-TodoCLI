8f65cf5f88273d414d62235c3eab3a4f083a80af
Mouthree <2115932130@qq.com>
2026-05-28 10:27:40 +0800

    改: ai的json完善, 以及new函数

--- src/ai.rs
+++ src/ai.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-
+///传给deepseek的json
 #[derive(Serialize)]
 struct ChatRequest {
     model: String,
@@ -10,13 +10,27 @@ struct ChatRequest {
     reasoning_effort: String,
 }
 
+///系统提示词和用户输入
 #[derive(Serialize, Debug, Deserialize, Clone)]
 struct Message {
     role: String,
     content: String
 }
 
+///是否开启思考
 #[derive(Serialize, Deserialize, Debug, Clone)]
 struct EnableType {
-    type: String
+    enable_type: String
+}
+
+impl ChatRequest {
+    pub fn new() -> Self {
+        ChatRequest { 
+            model: "deepseek-v4-lite".to_string(),
+            message: vec![Message { role: "system".to_string(), content: "提示词".to_string()}],
+            stream: false,
+            thinking: EnableType { enable_type: "disable".to_string() },
+            reasoning_effort: "high".to_string()
+        }
+    }
 }
